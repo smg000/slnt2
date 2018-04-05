20c07c357a0c8a4ba0b2e9b0f460e65fd4378c93
dunethevizsla <sean.graber@gmail.com>
2018-04-05 09:29:09 -0400

    Freaking Heroku

--- slant/settings.py
+++ slant/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # NOT SURE ABOUT THIS
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,7 +93,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
@@ -136,3 +136,8 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = 'slantapp/media/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env) # db_from_env = slantapp ???
