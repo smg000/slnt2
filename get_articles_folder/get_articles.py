0d33d99742a7b6f05fd50c5dbca404862be4fcd1
dunethevizsla <sean.graber@gmail.com>
2018-04-26 18:10:49 -0400

    Add url_prepend to Models.py.

--- get_articles_folder/get_articles.py
+++ get_articles_folder/get_articles.py
@@ -4,7 +4,9 @@ import datetime
 import math
 from newspaper import Article
 import numpy
+import os
 import pandas
+import psycopg2
 import re
 import requests
 import scrapy
@@ -14,6 +16,20 @@ from urllib.request import urlopen
 
 file_sites = '~/Desktop/SLNT/Python/sites.csv'
 sites_db = pandas.read_csv(file_sites, header=None, keep_default_na=False)
+
+def TBD(tbd):
+    # Environment variables
+    SLNT_DB_NAME = os.environ.get('SLNT_DB_NAME')
+    SLNT_DB_USER = os.environ.get('SLNT_DB_USER')
+    SLNT_DB_PASSWORD = os.environ.get('SLNT_DB_PASSWORD')
+
+    # Establish connection
+    conn = psycopg2.connect(host='ec2-54-163-240-54.compute-1.amazonaws.com', dbname=SLNT_DB_NAME, user=SLNT_DB_USER, password=SLNT_DB_PASSWORD, sslmode='require')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM slantapp_article")
+    records = cursor.fetchall()
+    for record in records:
+        print(record[1])
 
 def get_links(sites_db):
     urls = []
@@ -26,24 +42,29 @@ def get_links(sites_db):
         # TODO name columns in dataframe
         site_id = sites_db.ix[i][0]
         site = sites_db.ix[i][3]
+        print(site)
         keywords_include = sites_db.ix[i][4].split(',')
         keywords_exclude = sites_db.ix[i][5].split(',')
         url_prepend = sites_db.ix[i][6]
-        webpage = urlopen(site)
-        soup = BeautifulSoup(webpage, 'html5lib')
-        site_urls = []
+        try:
+            webpage = urlopen(site)
+            soup = BeautifulSoup(webpage, 'html5lib')
+            site_urls = []
 
-        # a_tags
-        a_tags = soup.find_all('a')
-        for a_tag in a_tags:
-            a_tag = a_tag.get('href')
-            if a_tag is not None and a_tag not in site_urls:
-                site_urls.append(a_tag)
-        for site_url in site_urls:
-            if any(keyword_include in site_url for keyword_include in keywords_include) and not any(keyword_exclude in site_url for keyword_exclude in keywords_exclude):
-                if site_url[0] == '/' and url_prepend != '':
-                    site_url = url_prepend + site_url
-                urls.append([site_id, site_url, date_stamp])
+            # a_tags
+            a_tags = soup.find_all('a')
+            for a_tag in a_tags:
+                a_tag = a_tag.get('href')
+                if a_tag is not None and a_tag not in site_urls:
+                    site_urls.append(a_tag)
+            for site_url in site_urls:
+                if any(keyword_include in site_url for keyword_include in keywords_include) and not any(keyword_exclude in site_url for keyword_exclude in keywords_exclude):
+                    if site_url[0] == '/' and url_prepend != '':
+                        site_url = url_prepend + site_url
+                    urls.append([site_id, site_url, date_stamp])
+        except:
+            print("Unable to scrape articles from %s." % (sites_db.ix[i][1]))
+            pass
 
     # Output file
     output_filename = 'urls_' + datetime_stamp + '.csv'
@@ -52,7 +73,7 @@ def get_links(sites_db):
         writer.writerows(urls)
     print('Success! Your URLs have been scraped!')
 
-# get_links(sites_db)
+get_links(sites_db)
 
 file_links = '~/PycharmProjects/slant/get_articles_folder/urls_04-19_08-45.csv'
 urls = pandas.read_csv(file_links, header=None, keep_default_na=False)
@@ -156,7 +177,7 @@ def analyze_articles(articles):
 #             # break
 # print(noun_adj_pairs)
 
-analyze_articles(articles)
+# analyze_articles(articles)
 
 # for i in range(1, articles.shape[0]):
 #     article = articles.ix[i][7]
--- slantapp/models.py
+++ slantapp/models.py
@@ -37,5 +37,6 @@ class Site(models.Model):
     url_full = models.URLField(max_length=500)
     url_keys_include = models.TextField()
     url_keys_exclude = models.TextField()
+    url_prepend = models.URLField(max_length=500 null=True, default='')
     def __str__(self):
         return "%s" % (self.publication_name)
