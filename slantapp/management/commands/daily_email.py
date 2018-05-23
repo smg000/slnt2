1fd8004a1359110d67df3a667f46c4ee109f6649
dunethevizsla <sean.graber@gmail.com>
2018-05-23 00:51:30 -0400

    Update padding around Disqus.

--- slantapp/management/commands/daily_email.py
+++ slantapp/management/commands/daily_email.py
@@ -1,28 +1,30 @@
 import datetime
-from django.core.mail import send_mail, BadHeaderError
+import os
+from django.core.mail import EmailMessage, BadHeaderError, send_mail
 from django.core.management.base import BaseCommand, CommandError
 from django.template.loader import render_to_string
-from slantapp.models import Issue, Article
+# from slantapp.models import Issue, Article
+from mailchimp3 import MailChimp
 
-# from mailchimp3 import MailChimp
-
-
-# Get email addresses from MailChimp
-# client = MailChimp(mc_api='YOUR_API_KEY', mc_user='YOUR_USERNAME')
-# return the first 100 member's email addresses for the list with id 123456
-# client.lists.members.all('123456', count=100, offset=0, fields="members.email_address")
 
 class Command(BaseCommand):
     help = 'Sends daily email to MailChimp subscribers.'
+
+    # Get email addresses from MailChimp
+    mailchimp_user = os.environ.get('MAILCHIMP_USER')
+    mailchimp_api = os.environ.get('MAILCHIMP_API')
+    client = MailChimp(mc_api=mailchimp_api, mc_user=mailchimp_user)
+    email_dictionary = client.lists.members.all('797ef6d1dc', get_all=True, fields="members.email_address")
+    email_list = []
+    for item in email_dictionary['members']:
+        email_list.append(item['email_address'])
 
     def add_arguments(self, parser):
         # parser.add_argument('--subject', type=str, required=True)
         pass
 
     def handle(self, *args, **options):
-
         issues = Issue.objects.filter(display=True).order_by('order')
-        # TODO Order articles by bias, from left to right
         articles = Article.objects.filter(display=True, issue__in=issues)
 
         message_plain = render_to_string(
@@ -42,6 +44,17 @@ class Command(BaseCommand):
         # to_email = 'sean.graber@gmail.com'
         # to_email = 'sean.m.graber.tu18@tuck.dartmouth.edu'
         # to_email = 'eleanortansey@gmail.com'
+
+        email = EmailMessage(
+            subject="the daily skeww | " + datetime.date.today().strftime("%B %d, %Y"),
+            body=message_plain,
+            from_email='Sean @ the skeww <hi@theskeww.com>',
+            to='Sean @ the skeww <hi@theskeww.com>',
+            bcc=email_list,
+            message_html=
+        )
+
+
 
         send_mail(
             subject=subject,
